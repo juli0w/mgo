ca6ed7227cb6d75a932eed079a5227ce79209286
Julio Budal <juli0w@hotmail.com>
2017-02-02 20:36:52 -0200

    remove bin from linked dirs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -23,7 +23,7 @@ set :repo_url, 'git@github.com:juli0w/mgo.git'
 set :branch, 'master'
 
 set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 # role-based syntax
 # ==================
