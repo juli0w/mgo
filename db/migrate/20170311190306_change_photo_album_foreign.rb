d4db7167ea26a1070f5be23227c1d3a4778289b9
Julio Budal <juli0w@hotmail.com>
2017-03-11 16:10:18 -0300

    fixing foreign

--- db/migrate/20170311190306_change_photo_album_foreign.rb
+++ db/migrate/20170311190306_change_photo_album_foreign.rb
@@ -1,7 +1,8 @@
 class ChangePhotoAlbumForeign < ActiveRecord::Migration[5.0]
   def change
     # change_table :photos do |t|
-      remove_foreign_key :photos, name: "photos_on_album_page_id"
+      remove_column :photos, :album_page_id
+      add_column :photos, :album_page_id, :integer
     # end
   end
 end
