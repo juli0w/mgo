6519f15431860b63d13fa736a0319a6a41fc501c
Julio Budal <juli0w@hotmail.com>
2017-03-11 16:05:56 -0300

    fixing foreign

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311161145) do
+ActiveRecord::Schema.define(version: 20170311190306) do
 
   create_table "album_pages", force: :cascade do |t|
     t.datetime "created_at", null: false
