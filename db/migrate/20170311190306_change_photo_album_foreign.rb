7ec40dc2dfd09d9ed5847525757a69b19a1eb974
Julio Budal <juli0w@hotmail.com>
2017-03-11 17:23:05 -0300

    fixing

--- db/migrate/20170311190306_change_photo_album_foreign.rb
+++
@@ -1,8 +0,0 @@
-class ChangePhotoAlbumForeign < ActiveRecord::Migration[5.0]
-  def change
-    # change_table :photos do |t|
-      remove_column :photos, :album_page_id
-      add_column :photos, :album_page_id, :integer
-    # end
-  end
-end
