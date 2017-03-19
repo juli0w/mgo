cd011ec4a4e6ac4c69f6b57ff8a0f2733a899b03
Julio Budal <juli0w@hotmail.com>
2017-03-18 23:08:54 -0300

    fixing promos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,16 +37,23 @@ $(document).ready(function(){
 
   $('.slick-sliding').slick({
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: false,
     responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        }
+      },
     {
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
-        infinite: true,
-        dots: true
       }
     },
     {
@@ -57,7 +64,7 @@ $(document).ready(function(){
       }
     },
     {
-      breakpoint: 430,
+      breakpoint: 460,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
