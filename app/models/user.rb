b55c2f292f1410689834d4a0ac0294e18dd3a296
Julio Budal <juli0w@hotmail.com>
2017-03-20 08:12:35 -0300

    already logged

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,15 +13,23 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails,
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+    if self.where(email: auth.info.email).exists?
+      return_user = self.where(email: auth.info.email).first
+      return_user.provider = auth.provider
+      return_user.uid = auth.uid
+    else
+      return_user = self.create do |user|
+         user.provider = auth.provider
+         user.uid = auth.uid
+         user.name = auth.info.name
+         user.image = auth.info.image
+         user.email = auth.info.email
+        #  user.oauth_token = auth.credentials.token
+        #  user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      end
     end
+
+    return_user
   end
 
   def self.new_with_session(params, session)
